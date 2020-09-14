6222dc47c7762ac741c5177563ca741bc7cf9781
LeaOpitz <lopireg@gmail.com>
2020-09-14 16:13:31 +0100

    Lea wrote nmds code

--- Code/starter_code.R
+++ Code/starter_code.R
@@ -14,13 +14,79 @@ library(goeveg)
 
 #load data ----
 b_plants <- read.csv("Data/raw_data_b_plants.csv") %>% 
-  rename(q_species = q.species)
+  rename(q_species = q.species) #rename variable
 c_plants <- read.csv("Data/raw_data_c_plants.csv") 
 soil <- read.csv("Data/raw_data_soil.csv")
 
 p_div <- read.csv("Data/PlantD.csv")
 i_div <- read.csv("Data/InsectD.csv")
 
+
+#write.csv2(species, file="test.csv")
+
+
 #join data
-plants <- bind_rows(b_plants, c_plants) %>%
-  dplyr::select(1:36)
+plants <- bind_rows(b_plants, c_plants) %>% #join
+  dplyr::select(1:36) %>% #remove last columns
+  dplyr::
+
+species <-  dplyr::select(plants,2:36) 
+species[is.na(species)] <- 0
+species = species[1:56,]
+
+### NMDS -----
+#check dimension
+dimcheckMDS(as.matrix(species), distance = "bray", k = 6, trymax = 20,
+            autotransform = TRUE)
+
+# Run the NMDS
+set.seed(2)  # Because the final result depends on the initial random placement of the points, we`ll set a seed to make the results reproducible
+NMDS3 <- metaMDS(species, k = 2, trymax = 100, trace = F, autotransform = FALSE, distance="bray")
+NMDS3
+
+# Check the stress
+stressplot(NMDS3)
+
+# Look at the results!
+plot(NMDS3)
+
+# nicer plot
+ordiplot(NMDS3, type = "n")
+orditorp(NMDS3, display = "species", col = "red", air = 0.01)
+orditorp(NMDS3, display = "sites", cex = 1.1, air = 0.01)
+
+## sites
+# Define a group variable (first 12 samples belong to group 1, last 12 samples to group 2)
+group = c(rep("Blackford", 28), rep("Craigmillar", 28))
+
+# Create a vector of color values with same length as the vector of group values
+colors = c(rep("red", 28), rep("blue", 28))
+
+# Plot convex hulls with colors based on the group identity
+ordiplot(NMDS3, type = "n")
+
+for(i in unique(group)) {
+  ordihull(NMDS3$point[grep(i, group),], draw="polygon",
+           groups = group[group == i],col = colors[grep(i,group)],label=F) } 
+
+orditorp(NMDS3, display = "species", col = "red", air = 0.01)
+orditorp(NMDS3, display = "sites", col = c(rep("red",12),  rep("blue", 12)), air = 0.01, cex = 1.25)
+
+## distance
+
+group = rep(c("0m", "1m", "7m", "14m"), 14)
+
+# Create a vector of color values with same length as the vector of group values
+colors = rep(c("yellow", "orange", "red", "purple"), 14)
+
+# Plot convex hulls with colors based on the group identity
+ordiplot(NMDS3, type = "n")
+
+for(i in unique(group)) {
+  ordihull(NMDS3$point[grep(i, group),], draw="polygon",
+           groups = group[group == i],col = colors[grep(i,group)],label=F) } 
+
+orditorp(NMDS3, display = "species", col = "red", air = 0.01)
+orditorp(NMDS3, display = "sites", col = c(rep("red",12),  rep("blue", 12)), air = 0.01, cex = 1.25)
+
+#take species off graph
