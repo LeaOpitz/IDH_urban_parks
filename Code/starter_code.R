ce5314802421dd17feddbc58bf746791d46f8460
LeaOpitz <lopireg@gmail.com>
2020-09-14 14:37:37 +0100

    Lea made starter code

---
+++ Code/starter_code.R
@@ -0,0 +1,15 @@
+# Field course in EES 2020
+# University of Edinburgh
+
+# Lea Opitz (s1757963)
+# Alessandra Cianfanelli (s1751650)
+
+
+# Libraries ----
+library(tidyverse)
+library(wesanderson)
+library(vegan)
+library(goeveg)
+
+
+#load data ----
